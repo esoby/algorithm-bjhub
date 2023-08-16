b2686ebe9b00468cfefde6bbf9dcc9b6ed181e84
소희 <138556584+esoby@users.noreply.github.com>
2023-08-17 00:21:57 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,9 +1,8 @@
 function solution(n) {
     const arr = [];
-    while (true){
-        arr.push(n % 3)
-        if (n < 3) break;
+    while (n){
+        arr.push(n % 3);
         n = parseInt(n / 3);
     }
-    return arr.reverse().map((v, i) => v * Math.pow(3, i)).reduce((a, c) => a+c, 0);
+    return arr.reverse().map((v, i) => v * Math.pow(3, i)).reduce((a, c) => a + c, 0);
 }
