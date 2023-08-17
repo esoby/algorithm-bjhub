7694867eed140968e35aabec419a5ae2277c0bbd
소희 <138556584+esoby@users.noreply.github.com>
2023-08-17 16:51:39 +0900

    [level 0] Title: k의 개수, Time: 27.03 ms, Memory: 39.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.js
@@ -1,8 +1,6 @@
 function solution(i, j, k) {
     let answer = '';
-    for(let idx=i;idx <= j; idx++){
-        answer += idx
-    }
+    for(let idx = i; idx <= j; idx++){ answer += idx; }
     return answer.split('').filter((e) => {
         return parseInt(e) === k ? true : false
     }).length
