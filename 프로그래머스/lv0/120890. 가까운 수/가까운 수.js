7c45dd5c4cf77946b9c02265c9b0743850edb324
소희 <138556584+esoby@users.noreply.github.com>
2023-08-19 01:47:43 +0900

    [level 0] Title: 가까운 수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,4 +1,4 @@
 function solution(array, n) {
-    let gap = array.map((v) => Math.abs(v - n)).sort((a, b)=> a-b)[0];
-    return array.indexOf(n - gap) !== -1 ? n - gap : n + gap
+    var answer = 0;
+    return answer;
 }
