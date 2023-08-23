da89f04adadab0bf2852e59d6ea2a8735684c92b
소희 <138556584+esoby@users.noreply.github.com>
2023-08-23 22:50:19 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,5 +1,7 @@
 function solution(food) {
     var str = '';
-    food.forEach((v, i) => { str += (i+'').repeat(parseInt(v / 2)); })
-    return str + '0' + str.split('').sort().reverse().join('');
+    food.forEach((v, i) => { 
+        str += (i+'').repeat(parseInt(v / 2)); 
+    })
+    return str + '0' + str.split('').reverse().join('');
 }
