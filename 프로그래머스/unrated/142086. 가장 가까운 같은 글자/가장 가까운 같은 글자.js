4b6e57b5b2ceb0e6a0f26a4ed6be10f6717e831f
소희 <138556584+esoby@users.noreply.github.com>
2023-09-01 20:29:21 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 2.10 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,27 +1,11 @@
-function solutionX(s) {
-    var answer = [];
-    
-    const myset = new Set();
-    let arr = s.split('')
-    arr.forEach((c, idx) => {
-        if(!myset.has(c)) {
-            answer.push(-1)
-            myset.add(c)
-        } else {
-            let i = 0;
-            for(i = idx - 1; i > 0; i--) if(c == arr[i]) break;
-            answer.push(idx - i)
-        }
-    })
-    return answer;
-}
-
 function solution(s) {
     var answer = [];
+    
     const map = new Map();
-    s.split('').forEach((c, i) => {
+    [...s].forEach((c, i) => {
         answer.push(map.get(c) > -1 ? i - map.get(c) : -1)
         map.set(c, i)
     })
+    
     return answer;
 }
