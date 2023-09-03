c866ce62ba8022242c4fd2f3a1d6418d8f8b171a
소희 <138556584+esoby@users.noreply.github.com>
2023-09-03 19:05:41 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -2,11 +2,11 @@ function solution(k, score) {
     const honor = [];
     
     return score.map(v => {
-        if( Math.min(...honor) < v || honor.length < k) {
+        if( honor[0] < v || honor.length < k) {
             if(honor.length === k) honor.shift();
             honor.push(v);
             honor.sort((a, b) => a - b);
         }
-        return Math.min(...honor);
+        return honor[0];
     })
 }
