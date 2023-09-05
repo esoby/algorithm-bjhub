33b2374d31e40e7a6e5e805dada5e71ada64cb3b
소희 <138556584+esoby@users.noreply.github.com>
2023-09-06 03:53:35 +0900

    [unrated] Title: 카드 뭉치, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,21 @@
+function solution(cards1, cards2, goal) {
+    let flag = true;
+    let trycard = 0;
+    goal.forEach(v => {
+        if(cards1[0] === v) {
+            cards1.shift(); 
+        }
+        else if(cards2[0] === v) {
+            cards2.shift();
+        }
+        else { 
+            if(trycard === Math.min(cards1.length, cards2.length)){
+                flag = false;
+                trycard = 0
+            }
+            trycard++;
+        }
+    }) 
+
+    return flag ? 'Yes' : 'No';
+}
