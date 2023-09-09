3b66877fc11e0f957eac86640fe1970b2edbd10f
소희 <138556584+esoby@users.noreply.github.com>
2023-09-10 03:31:48 +0900

    [level 1] Title: 소수 만들기, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,15 @@
+function isPrime(n) {
+    for(let i = 2; i <= n/2; i++) if (n % i === 0) return false;
+    return true
+}
+
+function solution(nums) {
+    let cnt = 0;
+    
+    for(let i = 0; i < nums.length; i++)
+        for(let j = i + 1; j < nums.length; j++)
+            for(let k = j + 1; k < nums.length; k++)
+                if(isPrime(nums[i] + nums[j] + nums[k])) cnt++;
+    
+    return cnt;
+}
