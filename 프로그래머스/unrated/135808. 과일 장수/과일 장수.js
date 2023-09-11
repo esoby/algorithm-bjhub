325205a70d816642522513ecd5aa967acd512982
소희 <138556584+esoby@users.noreply.github.com>
2023-09-11 21:27:40 +0900

    [unrated] Title: 과일 장수, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -2,15 +2,10 @@ function solution(k, m, score) {
     score.sort((a, b) => b - a)
     
     let sum = 0;
-    // while(score.length >= m){
-    //     let tmp = score.splice(0, m)
-    //     sum += Math.min(...tmp) * m;
-    // }
-    
     let count = Math.floor(score.length / m);
 
-    for(let i=1; i<=count; i++) {
-        sum += score[m*i-1] * m;  
+    for(let i = 1; i <= count; i++) {
+        sum += score[m * i - 1] * m;  
     }
     return sum;
 }
