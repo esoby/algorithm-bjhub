4d9703ab591d8ad869ce0dbaa73ce4f227ea8293
소희 <138556584+esoby@users.noreply.github.com>
2023-09-12 22:50:55 +0900

    [unrated] Title: 덧칠하기, Time: 3.46 ms, Memory: 39.9 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -1,12 +1,19 @@
 function solution(n, m, sec) {
-    let cnt = 0;
+    let cnt = 1;
 
+    // for(const el of sec){
+    //     if(el === 0) continue;
+    //     let stand = el + m;
+    //     cnt++;
+    //     for(let j = sec.indexOf(el); j < sec.length; j++) 
+    //         if(sec[j] < stand) sec[j] = 0;
+    // }
+    let stand = sec[0] + m - 1
     for(const el of sec){
-        if(el === 0) continue;
-        let stand = el + m;
-        cnt++;
-        for(let j = sec.indexOf(el); j < sec.length; j++) 
-            if(sec[j] < stand) sec[j] = 0;
+        if (stand < el) {
+            stand = el + m - 1
+            cnt++;
+        }
     }
     
     return cnt;
