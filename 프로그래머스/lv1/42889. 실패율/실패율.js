e5c8293a8f8f0dcef6a7a18ed02840cf761fe3b1
소희 <138556584+esoby@users.noreply.github.com>
2023-09-13 19:15:45 +0900

    [level 1] Title: 실패율, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,14 +1,14 @@
 function solution(N, stages) {
     const fail = []
+    
     for(let curr = 1; curr <= N; curr++){
         let cnt1 = 0, cnt2 = 0;
         stages.forEach(v => {
             if(curr === v) cnt1++;
             if(curr <= v) cnt2++;
         })
-        fail.push(cnt1 / cnt2)
+        fail.push([curr, cnt1 / cnt2])
     } 
-    let map = new Map()
-    fail.forEach((v, i) => { map.set(i, v) })
-    return [...map].sort((a, b) => b[1] - a[1]).map(v => v[0] + 1)
+    
+    return fail.sort((a, b) => b[1] - a[1]).map(v => v[0])
 }
