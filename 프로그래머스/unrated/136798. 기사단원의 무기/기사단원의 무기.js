eaf57b30dfaccc00c2bd8ec5940618c2e19e583d
소희 <138556584+esoby@users.noreply.github.com>
2023-09-13 20:32:40 +0900

    [unrated] Title: 기사단원의 무기, Time: 2.64 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,11 +1,10 @@
 function solution(number, limit, power) {
-    let arr = new Array(number).fill(0)
     let sum = 0;
-    arr.forEach((v, idx) => {
+    for(let idx = 1; idx <= number; idx++) {
         let cnt = 0;
-        if((idx+1) % Math.sqrt(idx+1) === 0) cnt++;
-        for(let i = 1; i < Math.sqrt(idx+1); i++) if((idx+1) % i === 0) cnt += 2;
+        if(idx % Math.sqrt(idx) === 0) cnt++;
+        for(let i = 1; i < Math.sqrt(idx); i++) if(idx % i === 0) cnt += 2;
         if(cnt > limit) sum += power; else sum += cnt; 
-    })
+    }
     return sum;
 }
