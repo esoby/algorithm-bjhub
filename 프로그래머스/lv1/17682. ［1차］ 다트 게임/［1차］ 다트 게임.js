3842cec8727e1381bc3bc82b493fda11330b111d
소희 <138556584+esoby@users.noreply.github.com>
2023-09-19 01:36:31 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -0,0 +1,29 @@
+function solution(dartResult) {
+    let answer = [];
+    let curr = 0;
+    
+    dartResult = dartResult.split('');
+    dartResult.forEach((v, i) => {
+        if('0123456789'.indexOf(v) > -1) {
+            let idx = i+1;
+            
+            if('0123456789'.indexOf(dartResult[i-1]) > -1) return false;
+            
+            for(let j = i + 1; j < dartResult.length; j++){
+                if('0123456789'.indexOf(dartResult[j]) === -1) {idx = j; break;}
+            }
+            v = dartResult.slice(i, idx).join('')
+            switch (dartResult[idx]){
+                case 'S' : answer.push(parseInt(v)); curr++; break;
+                case 'D' : answer.push(v * v); curr++; break;
+                case 'T' : answer.push(v * v * v); curr++; break;
+            }
+        } else if(v === '*'){
+            if(curr-1 > -1) answer[curr-1] = 2 * answer[curr-1];
+            if(curr-2 > -1) answer[curr-2] = 2 * answer[curr-2];
+        } else if(v === '#'){
+            if(curr-1 > -1) answer[curr-1] = -1 * answer[curr-1];
+        } 
+    })
+    return answer.reduce((a,c) => a + parseInt(c), 0);
+}
