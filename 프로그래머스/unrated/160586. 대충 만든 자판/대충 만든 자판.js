377204ce1d247b0030e8f417677a8d82d03f82ba
소희 <138556584+esoby@users.noreply.github.com>
2023-09-26 01:49:44 +0900

    [unrated] Title: 대충 만든 자판, Time: 1.09 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.js
@@ -0,0 +1,16 @@
+function solution(keymap, targets) {
+    let map = new Map();
+    
+    keymap.forEach(str => {
+        str.split('').forEach((v, i) => {
+            map.set(v, Math.min(i + 1, map.get(v) ?? i + 1));
+        })
+    })
+    targets = targets.map(tg => {
+        tg = tg.split('').map(v => map.get(v)).reduce((a,c) => a + c,0)
+        if (!tg) return -1
+        else return tg
+        
+    })
+    return targets;
+}
