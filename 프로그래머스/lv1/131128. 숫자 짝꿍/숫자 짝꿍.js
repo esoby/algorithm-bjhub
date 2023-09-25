647b54730bc549190ab8345b5680f70c0b75ebd5
소희 <138556584+esoby@users.noreply.github.com>
2023-09-26 00:17:43 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -1,13 +1,14 @@
 function solution(X, Y) {
   const arrX = X.split('');
   const arrY = Y.split('');
+    
   let result = '';
-  
   for(let i = 9; i >= 0; i--) {
     const cntX = arrX.filter(v => v == i).length;
     const cntY = arrY.filter(v => v == i).length;
-    if(cntX && cntY) result += String(i).repeat(Math.min(cntX, cntY));
+
+    result += String(i).repeat(Math.min(cntX, cntY));
   }
     
-    return !result ? '-1' : !parseInt(result) ? '0' : result ;
+    return !result ? '-1' : !parseInt(result) ? '0' : result;
 }
