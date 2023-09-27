1f38eac20dd9621dda2358d1e4db1181336846cb
소희 <138556584+esoby@users.noreply.github.com>
2023-09-28 00:44:35 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.80 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,8 +1,6 @@
 function solution(A,B){
-    var answer = 0;
-    
-    A.sort((a, b) => a - b)
-    B.sort((a, b) => b - a)
+    A.sort((a, b) => b - a)
+    B.sort((a, b) => a - b)
     
     return A.map((v, i) => v * B[i]).reduce((a, c) => a + c, 0)
 }
