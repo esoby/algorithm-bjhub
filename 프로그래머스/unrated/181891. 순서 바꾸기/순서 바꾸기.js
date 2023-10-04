d7ea03e4729809707bdbd0ea8aa2e34af8b55495
소희 <138556584+esoby@users.noreply.github.com>
2023-10-05 00:33:40 +0900

    [unrated] Title: 순서 바꾸기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.js
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.js
@@ -1,5 +1,4 @@
 function solution(num_list, n) {
-    const add = num_list.slice(0, n);
-    num_list.splice(0, n);
-    return [...num_list, ...add];
+    num_list.push(...num_list.splice(0, n));
+    return num_list
 }
