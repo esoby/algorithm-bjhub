69826afa135831247ae592b55ab8becf207f4052
소희 <138556584+esoby@users.noreply.github.com>
2023-10-10 00:08:36 +0900

    [level 2] Title: 숫자의 표현, Time: 1.62 ms, Memory: 36.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,18 @@
+function solution(n) {
+    let cnt = 0;
+    if (n == 1) return 1;
+    for(let i = 1; i < n; i++){
+        let total = 0
+        for(let j = i; j <= n; j++){
+            total += j
+            if(total > n) break;
+            if(total == n){
+                cnt++;
+                break;
+            }
+        }
+    }
+    
+    
+    return cnt + 1;
+}
