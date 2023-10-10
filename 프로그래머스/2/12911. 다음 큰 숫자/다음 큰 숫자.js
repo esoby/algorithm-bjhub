878506714eb2387b27fa45e43563c2bd0225fc22
소희 <138556584+esoby@users.noreply.github.com>
2023-10-10 09:30:05 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,9 +1,8 @@
 function solution(n) {
     let res = n + 1
-    for( ; ; res++){
+    for( ; ; res++)
         if (n.toString(2).split('').filter(v => v == 1).length ===
         res.toString(2).split('').filter(v => v == 1).length)
             break;
-    }
     return res;
 }
