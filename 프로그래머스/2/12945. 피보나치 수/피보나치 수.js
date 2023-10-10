977c8df2a8382907f7fd6181c1985a8cb7a97f23
소희 <138556584+esoby@users.noreply.github.com>
2023-10-10 09:29:42 +0900

    [level 2] Title: 피보나치 수, Time: 4.34 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -4,9 +4,8 @@ function solution(n) {
     arr.push(0)
     arr.push(1)
     
-    for(let i = 2; i < n + 1; i++){
-        arr.push((arr[i-1] + arr[i-2]) % 1234567)
-    }
+    for(let i = 2; i < n + 1; i++)
+        arr.push((arr[i-1] + arr[i-2]) % 1234567);
     
     return arr[n]
 }
