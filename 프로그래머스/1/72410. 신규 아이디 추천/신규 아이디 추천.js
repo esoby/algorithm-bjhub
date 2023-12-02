b18d2f9d500f3b9bc3bcad14ab3968a9eacd5da1
소희 <138556584+esoby@users.noreply.github.com>
2023-12-02 23:06:57 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -0,0 +1,19 @@
+function solution(new_id) {
+    // 1, 2
+    new_id = new_id.toLowerCase().replaceAll(/[^a-z0-9\-\_\.]/g,'')
+    // 3
+    while(new_id.includes('..')) new_id = new_id.replaceAll('..', '.');
+    // 4
+    new_id = new_id.split('');
+    if(new_id[0] === '.') new_id.shift();
+    if(new_id[new_id.length - 1] === '.') new_id.pop();
+    // 5
+    if(!new_id[0]) new_id.push('a');
+    // 6
+    while(new_id.length > 15) new_id.pop();
+    if(new_id[new_id.length - 1] === '.') new_id.pop();
+    // 7
+    while(new_id.length < 3) new_id.push(new_id[new_id.length - 1])
+    
+    return new_id.join('')
+}
