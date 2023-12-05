9b001ad6bd9f3f99ea4959a7099bfc4bf571fce2
소희 <138556584+esoby@users.noreply.github.com>
2023-12-05 11:43:13 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.74 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
@@ -1,32 +1,25 @@
 function solution(numbers, hand) {
     var answer = '';
+    
     let left = [1, 4]
     let right = [3, 4]
     
-    numbers.forEach((v, i) => {    
-        switch (v) {
-            case 1 : case 4 : case 7 :
-                left = [1, parseInt(v / 3) + 1];
-                answer += 'L';
-                break;
-            case 3 : case 6 : case 9 :
-                right = [3, parseInt(v / 3)];
-                answer += 'R';
-                break;
-            default :
-                let des = [2, v ? parseInt(v / 3) + 1 : 4]
-                let l = des.reduce((a, c, i) => a + Math.abs(c - left[i]), 0)
-                let r = des.reduce((a, c, i) => a + Math.abs(c - right[i]), 0)
+    const setCoor = (ans, coor) => {
+        answer += ans;
+        ans === 'L'? left = coor : right = coor;
+    }
+    
+    numbers.forEach(num => {
+        if ('147'.includes(num)) setCoor('L', [1, parseInt(num / 3) + 1]);
+        else if ('369'.includes(num)) setCoor('R', [3, parseInt(num / 3)]);
+        else {
+            let des = [2, num ? parseInt(num / 3) + 1 : 4]
+            let l = des.reduce((a, c, i) => a + Math.abs(c - left[i]), 0)
+            let r = des.reduce((a, c, i) => a + Math.abs(c - right[i]), 0)
                 
-                if (l > r || (l === r && hand === 'right')) {
-                    right = des;
-                    answer += 'R';
-                }
-                else if (l < r || (l === r && hand === 'left')) {
-                    left = des;
-                    answer += 'L';
-                }
-            }
+            if (l > r || (l === r && hand === 'right')) setCoor('R', des);
+            else if (l < r || (l === r && hand === 'left')) setCoor('L', des);
+        }
     })
     return answer;
 }
