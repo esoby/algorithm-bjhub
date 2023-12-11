15caff2cf53f2f770744218eb0d0c7a301ff4b5c
소희 <138556584+esoby@users.noreply.github.com>
2023-12-11 11:16:10 +0900

    [level 1] Title: 공원 산책, Time: 0.44 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.js
@@ -0,0 +1,48 @@
+function solution(park, routes) {
+    let res = [0, 0];
+    let op = ''
+    let n = 0
+    
+    // 시작 지점 찾기
+    park.forEach((v, i) => {
+        v.split('').forEach((val, idx) => {
+            if (val === 'S') res = [i, idx]
+        })
+    })
+    
+    routes.forEach(route => {
+        [op, n] = route.split(' ')
+        let tmp = res
+        n = parseInt(n)
+        
+        if (op === 'E' && res[1] + n < park[0].length){ 
+            let flag = true
+            for (let i = 1; i <= n; i++){
+                if (park[res[0]][res[1] + i] === 'X') flag = false
+            }
+            if(flag) res[1] += n
+        }
+        else if (op === 'W' && res[1] - n >= 0) {
+            let flag = true
+            for (let i = 1; i <= n; i++){
+                if (park[res[0]][res[1] - i] === 'X') flag = false
+            }
+            if(flag) res[1] -= n
+        } 
+        else if (op === 'N' && res[0] - parseInt(n) >= 0) {
+            let flag = true
+            for (let i = 1; i <= n; i++){
+                if (park[res[0] - i][res[1]] === 'X') flag = false
+            }
+            if(flag) res[0] -= n
+        } 
+        else if (op === 'S' && res[0] + n < park.length){
+            let flag = true
+            for (let i = 1; i <= n; i++){
+                if (park[res[0] + i][res[1]] === 'X') flag = false
+            }
+            if(flag) res[0] += n
+        }
+    })
+    return res;
+}
