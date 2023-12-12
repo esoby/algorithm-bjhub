91706c6bbbd399910888cdbae40c0c12a953b95c
소희 <138556584+esoby@users.noreply.github.com>
2023-12-12 13:35:21 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.45 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -3,11 +3,16 @@ function solution(today, terms, privacies) {
     today = today[0] * 28 * 12 + today[1] * 28 + today[2]
     
     const obj_terms = {}
-    terms.forEach(v => obj_terms[v.split(' ')[0]] = parseInt(v.split(' ')[1]) * 28)
+    terms.forEach(v => {
+        v = v.split(' ')
+        obj_terms[v[0]] = parseInt(v[1]) * 28
+    })
     
     privacies = privacies.map((v, i) => {
-        let date = v.split(' ')[0].split('.').map(Number)
-        return date[0] * 28 * 12 + date[1] * 28 + date[2] + obj_terms[v.split(' ')[1]]
+        v = v.split(' ')
+        let date = v[0].split('.').map(Number)
+        return date[0] * 28 * 12 + date[1] * 28 + date[2] + obj_terms[v[1]]
     })
+    
     return privacies.map((v, i) => v - today <= 0 ? i + 1 : 0).filter(v => v)
 }
