3bcf0abeee2aa8322b0a0ac0ea87fc4dff0149a8
소희 <138556584+esoby@users.noreply.github.com>
2023-12-14 00:49:09 +0900

    [level 1] Title: 달리기 경주, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -1,17 +1,15 @@
 function solution(players, callings) {
     const map = new Map()
     players.forEach((player, idx) => map.set(player, idx))
-
     
     callings.forEach(call => {
         const idx = map.get(call);
-
-        map.set(call, idx - 1)
+        
         map.set(players[idx - 1], idx)
+        map.set(call, idx - 1)
         players[idx] = players[idx - 1];
         players[idx - 1] = call;
 
     })
-
     return players;
 }
