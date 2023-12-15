a9218c459f31ec1bffdcc52d6770b95e0fa0af13
소희 <138556584+esoby@users.noreply.github.com>
2023-12-16 01:03:24 +0900

    [level unrated] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
+++ 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -1,21 +1,17 @@
-function solution(band, hp, attacks) {
-    const sec = attacks[attacks.length-1][0]
-    const max_hp = hp
-    let cnt = 0
-
-    for (let i = 0; i <= sec; i++){
-        if (attacks[0][0] === i) {
-            hp -= attacks.shift()[1]
-            cnt = 0
-            if (hp <= 0) return -1
-        } else if (cnt === band[0] - 1) {
-            hp += band[2] + band[1]
-            cnt = 0
-        } else {
-            hp += band[1]
-            cnt += 1
-        }
-        if (hp > max_hp) hp = max_hp
+function solution(bandage, 현재체력, attacks) {
+    const 최대체력 = 현재체력
+    let 직전공격시간 = 0
+    let  [시전시간, 초당회복량, 추가회복량] = bandage
+    
+    for (let [공격시간, 피해량] of attacks) {
+        let 회복시간 = 공격시간 - 직전공격시간 - 1
+        현재체력 += 초당회복량 * 회복시간 + 추가회복량 * (회복시간 / 시전시간 >= 1 ? parseInt(회복시간 / 시전시간) : 0)
+        if (현재체력 > 최대체력) 현재체력 = 최대체력
+        
+        현재체력 -= 피해량
+        if (현재체력 <= 0) return -1
+        
+        직전공격시간 = 공격시간
     }
-    return hp
+    return 현재체력
 }
