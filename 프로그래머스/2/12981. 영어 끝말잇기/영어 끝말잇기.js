38ef8fbe6aff497f6b3d8aedd971051d0cfbcb76
소희 <138556584+esoby@users.noreply.github.com>
2023-12-17 19:14:31 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -0,0 +1,12 @@
+function solution(n, words) {
+    const ans = [words[0]];
+    
+    for (let idx = 1; idx < words.length; idx++) {
+        let word = words[idx]
+        if (ans.indexOf(word) === -1 && 
+            words[idx - 1][words[idx - 1].length - 1] === word[0]) 
+            ans.push(word);
+        else return [(idx + 1) % n === 0 ? n : (idx + 1) % n, Math.ceil((idx + 1) / n)];
+    }
+    return [0, 0];
+}
