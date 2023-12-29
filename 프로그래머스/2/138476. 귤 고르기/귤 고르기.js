f80ee35a94cd141001b007f71b37aed478a2513e
소희 <138556584+esoby@users.noreply.github.com>
2023-12-29 23:17:29 +0900

    [level 2] Title: 귤 고르기, Time: 59.99 ms, Memory: 50.6 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,14 +1,11 @@
 function solution(k, tangerine) {
-    let cnt = 0
     let obj = {}
     tangerine.forEach(v => obj[v - 1] ? obj[v - 1]++ : obj[v - 1] = 1)
     obj = Object.values(obj).sort((a, b) => a - b)
     
-    let ans = 0
-    while (cnt < k) {
-        cnt += obj.pop()
-        ans++
-    }
+    let cnt = 0
+    let level = obj.length
+    while (cnt < k) cnt += obj.pop()
 
-    return ans;
+    return level - obj.length;
 }
