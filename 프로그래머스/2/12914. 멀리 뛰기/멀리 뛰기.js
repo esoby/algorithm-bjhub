e1db6cb1238cd139c4e2e5d8840a94ca0bd6f0d7
소희 <138556584+esoby@users.noreply.github.com>
2023-12-30 10:13:54 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.56 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,8 +1,11 @@
 function solution(n) {
-    let a = 1, b = 2;
-    for(let i = 3; i <= n; i++) {
-        [a, b] = [b, a];
-        b = (a + b) % 1234567;
+    let a = 1
+    let b = 2
+    
+    for (let i = 3; i <= n; i++) {
+        [a, b] = [b, a]
+        b = (a + b) % 1234567
     }
-    return n === 1 ? n : b;
+    
+    return n === 1 ? n : b
 }
