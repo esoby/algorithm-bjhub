c5f27eadc28e8f3004cb904f81c666177ccc9fb6
소희 <138556584+esoby@users.noreply.github.com>
2024-01-08 22:46:34 +0900

    [level 2] Title: H-Index, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,8 +1,6 @@
 function solution(citations) {
     let arr = [0]
     
-    citations.sort((a, b) => b - a)
-    
     citations.forEach(v => {
         if(v >= citations.filter(ct => ct >= v).length)
             arr.push(citations.filter(ct => ct >= v).length)
