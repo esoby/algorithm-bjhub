ff69da226bf3f8ca2965d0d1a9df20fcddda73d1
소희 <138556584+esoby@users.noreply.github.com>
2024-01-11 23:21:28 +0900

    [level Lv.2] Title: 튜플, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/Lv.2/64065. 튜플/튜플.js
+++ 프로그래머스/Lv.2/64065. 튜플/튜플.js
@@ -1,19 +1,10 @@
 function solution(s) {
-    s = s.replaceAll('{', ' ')
-        .replaceAll('}', ' ')
-        .split(' ')
-        .filter(v => v !== '' && v !== ',')
-        .map(v => v.split(','))
-        .map(v => v.map(n => parseInt(n)))
-        .sort((a, b) => a.length - b.length)
+    s = s.slice(2, s.length-2).split('},{').map(v => v.split(','))
+        .map(v => v.map(n => parseInt(n))).sort((a, b) => a.length - b.length)
     
     const ans = []
     
-    s.forEach(v => {
-        v.forEach(n => {
-            if(ans.indexOf(n) === -1) ans.push(n)
-        })
-    })
+    s.forEach(v => v.forEach(n => ans.indexOf(n) === -1 ? ans.push(n) : ''))
     
     return ans
 }
