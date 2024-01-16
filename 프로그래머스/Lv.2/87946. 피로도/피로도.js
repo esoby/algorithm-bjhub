a9d55b36e095a0993ea99690644e2e124d02ae56
소희 <138556584+esoby@users.noreply.github.com>
2024-01-16 15:52:43 +0900

    [level Lv.2] Title: 피로도, Time: 0.35 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/Lv.2/87946. 피로도/피로도.js
+++ 프로그래머스/Lv.2/87946. 피로도/피로도.js
@@ -1,16 +1,15 @@
 function solution(k, dungeons) {
-    let answer = 0;
+    let ans = 0
   
     function dfs(k, visited){
-        if(k < 0) return;
-        answer = Math.max(answer, visited.length);
+        if(k < 0) return
+        ans = Math.max(ans, visited.length)
         
-        for(let i = 0; i < dungeons.length; i++) {
-            if(!visited.includes(i) && dungeons[i][0] <= k) {
-                dfs(k - dungeons[i][1], [...visited, i]);
-            }
-        }
+        for(let i = 0; i < dungeons.length; i++)
+            if(!visited.includes(i) && dungeons[i][0] <= k)
+                dfs(k - dungeons[i][1], [...visited, i])
     }
-    dfs(k, []);
-    return answer;
+    
+    dfs(k, [])
+    return ans
 }
