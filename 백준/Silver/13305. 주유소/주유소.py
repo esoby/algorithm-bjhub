76a6ff321bbe54cc8993e2ecbe2f4de83311cfde
소희 <138556584+esoby@users.noreply.github.com>
2024-01-18 11:38:41 +0900

    [Silver III] Title: 주유소, Time: 128 ms, Memory: 46788 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -3,12 +3,12 @@ n = int(input())
 roads = list(map(int, input().split()))
 costs = list(map(int, input().split()))
 
-min_cost = min(costs[:n-1])
-res = 0
-for i in range(n - 1):
-    if costs[i] == min_cost:
-        res += sum(roads[i:]) * min_cost
-        break
-    res += costs[i] * roads[i]
+res = costs[0] * roads[0]
+for i in range(1, n - 1):
+    if costs[i] > costs[i - 1]:
+        res += costs[i - 1] * roads[i]
+        costs[i] = costs[i - 1]
+    else:
+        res += costs[i] * roads[i]
 
 print(res)
