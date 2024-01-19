72926de984054527bfa7147d2a8cf8f3e33abd8a
소희 <138556584+esoby@users.noreply.github.com>
2024-01-19 09:24:36 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.46 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -1,33 +1,25 @@
 function solution(str1, str2) {
-    let arr1 = []
-    let arr2 = []
+    let arr1 = strToArr(str1)
+    let arr2 = strToArr(str2)
     
-    str1 = str1.toLowerCase()
-    str2 = str2.toLowerCase()
-    
-    for(let i = 0; i < str1.length - 1; i++)
-        if(str1.slice(i, i+2).search(/[^a-z]/) == -1)
-            arr1.push(str1[i] + str1[i + 1])
-    
-    for(let i = 0; i < str2.length - 1; i++)
-        if(str2.slice(i, i+2).search(/[^a-z]/) == -1)
-            arr2.push(str2[i] + str2[i + 1])
-
+    function strToArr(str){
+        let arr = []
+        for(let i = 0; i < str.length - 1; i++)
+            if(str.slice(i, i+2).search(/[^A-Za-z]/) == -1)
+                arr.push((str[i] + str[i + 1]).toLowerCase())
+        return arr
+    }
     
     let inter = 0
     let union = arr2.length
         
-    for(let i = 0; i < arr1.length; i++){
-        let flag = inter
-        for(let j = 0; j < arr2.length; j++){
-            if(arr1[i] === arr2[j]){
-                arr2[j] = '/'
-                inter++
-                break
-            }
-        }
-        if(flag == inter) union++
-    }
-    if (!inter && !union) return 65536
-    return Math.floor(inter / union * 65536)
+    arr1.forEach(v =>{
+        idx = arr2.indexOf(v)
+        if(idx > -1){
+            arr2[idx] = '/'
+            inter++
+        } else union++
+    })
+
+    return union === 0 ? 65536 : Math.floor(inter / union * 65536);
 }
