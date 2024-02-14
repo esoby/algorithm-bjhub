55af0fc9cd6b1aa6c5d203ad34f49c612b4543d1
소희 <138556584+esoby@users.noreply.github.com>
2024-02-14 09:54:14 +0900

    [level 2] Title: 방문 길이, Time: 57.88 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -1,26 +1,24 @@
 function solution(dirs) {
     let answer = 0;
+    const visited = []
+    const curr = [0, 0]
     
-    let visited = []
-    let curr = [0, 0]
     dirs.split('').forEach(dir => {
-        let tmp = curr.slice()
+        let before = curr.slice()
         if (dir === 'U' && curr[1] + 1 <= 5) curr[0] += 0, curr[1] += 1
         else if (dir === 'D' && curr[1] - 1 >= -5) curr[0] += 0, curr[1] -= 1
         else if (dir === 'R' && curr[0] + 1 <= 5) curr[0] += 1, curr[1] += 0
         else if (dir === 'L' && curr[0] - 1 >= -5) curr[0] -= 1, curr[1] += 0
-
-        if (JSON.stringify(curr) !== JSON.stringify(tmp)) {
+        
+        if (!(before[0] === curr[0] && before[1] === curr[1])) {
             let flag = true
             visited.forEach(visit => {
-                if (JSON.stringify(visit) === JSON.stringify(tmp.concat(curr))
-                    || JSON.stringify(visit) === JSON.stringify(curr.concat(tmp))) 
-                    flag = false
+                if (JSON.stringify(visit) === JSON.stringify(before.concat(curr))) flag = false
             })
-
             if (flag) {
                 answer++
-                visited.push(tmp.concat(curr))
+                visited.push(before.concat(curr))
+                visited.push(curr.concat(before))
             }
         }
     })
