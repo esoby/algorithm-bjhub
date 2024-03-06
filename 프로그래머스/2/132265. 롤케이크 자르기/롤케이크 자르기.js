cef01252e26bb9c0e7f545a06f0d41ef1f998b63
소희 <138556584+esoby@users.noreply.github.com>
2024-03-06 13:43:33 +0900

    [level 2] Title: 롤케이크 자르기, Time: 18.53 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -1,29 +1,23 @@
 function solution(topping) {
-    let answer = 0;
-    let firstPos = Array(10001).fill(-1);
-    let rightTopping = Array(topping.length).fill(0);
-    let rightToppingCount = 0;
-
+    let answer = 0
+    let pos = Array(10001).fill(-1)
+    let rightCnt = Array(topping.length).fill(0)
+    let cnt = 0
     for (let i = topping.length - 1; i >= 0; i--) {
-        if (firstPos[topping[i]] === -1) {
-            firstPos[topping[i]] = i;
-            rightToppingCount++;
+        if (pos[topping[i]] === -1) {
+            pos[topping[i]] = i
+            cnt++
         }
-        rightTopping[i] = rightToppingCount;
+        rightCnt[i] = cnt
     }
-
-    let leftToppingCount = 0;
-    firstPos.fill(-1);
-
+    cnt = 0
+    pos.fill(-1)
     for (let i = 1; i < topping.length; i++) {
-        if (firstPos[topping[i-1]] === -1) {
-            firstPos[topping[i-1]] = i;
-            leftToppingCount++;
+        if (pos[topping[i-1]] === -1) {
+            pos[topping[i-1]] = i
+            cnt++
         }
-        if (leftToppingCount === rightTopping[i]) {
-            answer++;
-        }
+        if (cnt === rightCnt[i]) answer++
     }
-
-    return answer;
+    return answer
 }
