f710c3f406f744f33307c3e1257746613b9679c0
소희 <138556584+esoby@users.noreply.github.com>
2024-03-12 20:34:08 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 11.88 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -1,25 +1,15 @@
 function solution(files) {
     files = files.map(file => {
-        let tmp = file.toLowerCase()
-        let head = ''
-        while('abcdefghijklmnopqrstuvwxyz- .'.includes(tmp[0])){
-            head += file[0]
-            file = file.slice(1)
-            tmp = tmp.slice(1)
-        }
-        let num = ''
-        while('0123456789'.includes(tmp[0])){
-            num += file[0]
-            file = file.slice(1)
-            tmp = tmp.slice(1)
-        }
-        return [head, num, file]
+        const match = file.match(/^([^\d]+)(\d+)/)
+        const tail = file.slice(match[1].length + match[2].length)
+        return [match[1], match[2], tail]
     })
     
     files.sort((a, b) => {
         let res = a[0].toLowerCase().localeCompare(b[0].toLowerCase())
         if (res == 0) return parseInt(a[1]) - parseInt(b[1])
         return res  
-    });
+    })
+    
     return files.map(v => v.join(''));
 }
